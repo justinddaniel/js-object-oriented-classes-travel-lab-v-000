2efee6f0555961982167da68560c5accca14347f
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 21:33:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = startDate
+  }
+  yearsExperienceFromBeginningOf(startDate, endDate) {return endDate - startDate;}
+}
