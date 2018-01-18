daf40c8c25f91f85a5818decbad2e447a377c86e
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 21:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ class Driver {
   }
   yearsExperienceFromBeginningOf(startDate, endDate) {return endDate - startDate;}
 }
+
+class Route {
+  let beginningLocation = {};
+  let endingLocation = {}
+  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+  
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  blocksTravelled(beginningLocation, endingLocation) {return Math.abs((parseInt(endingLocation.vertical)) - (parseInt(beginningLocation.vertical))); }
+}
