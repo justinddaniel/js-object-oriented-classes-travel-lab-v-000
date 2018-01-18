fc377b5af40b12aa54dacdba9f20474440ec726c
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 21:56:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class Driver {
   constructor(name, startDate) {
     this.name = name
-    this.startDate = new Date(startDate)
+    this.startDate = setFullYear
   }
-  yearsExperienceFromBeginningOf(startDate, endDate) {return endDate - startDate;}
+  yearsExperienceFromBeginningOf(endDate) {return endDate - this.startDate;}
 }
 
 class Route {
