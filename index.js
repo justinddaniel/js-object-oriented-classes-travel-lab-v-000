7299c1d66a553da5a8ec6c73212f88059a500e69
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 21:47:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Route {
   let beginningLocation = {};
   let endingLocation = {}
   let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-  
+
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
